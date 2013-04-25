db88c89aa876664cda8a131f291fa87c0792eb0b
Randy Hembrador <randy.hembrador@gmail.com>
2013-04-25 15:47:46 -0700

    Removed backup copy of site
    FTP automatically downloaded the site copy to the git folder. I zipped
    it and backed it up elsewhere.

--- randyhembrador.com/wp-content/themes/hembradorlab/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" class="fancySearch">
-  <div>
-	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" title="Search" class="fancybox.ajax tip" />
-	<!--input type="submit" id="searchsubmit" value="Search" /-->
-  </div>
-</form>
--- randyhembrador.com/wp-content/themes/notes-blog/readme.txt
+++
@@ -1,7 +0,0 @@
-This theme was created by THORD DANIEL HEDENGREN [http://tdh.me]
-
-Yes, there will be more here later. For now:
-
-HTTP://NOTESBLOG.COM
-
-Notes Blog will be open source forever!
